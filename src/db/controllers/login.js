eed4007c9ffa8a5537da336e3c6e2de5a0b3ff84
kie <179924858+kiieee@users.noreply.github.com>
2024-11-14 12:34:51 +0000

    pushed previous person of 2 files

--- src/db/controllers/login.js
+++ src/db/controllers/login.js
@@ -1,27 +1,27 @@
-    require("dotenv").config();
-    const jwt = require("jsonwebtoken");
-
-    function login(req,res) {
-        try {
-            const privateKey = process.env.JWT_SECRET_KEY
-            const expirationTime = 2*24*60*60
-            const payload = {
-                email:req.body.email,
-                username:req.body.username
-            };
-            const options = {
-                expiresIn: expirationTime
-            };
-            const token = jwt.sign(payload,privateKey,options);
-            console.log(token);
-            res.status(200).json({
-                message: `user ${req.body.username} has successfully logged in`,
-                token: token
-            })
-        }  catch(error) {
-            console.log(error)
-            res.status(500).json(error)
-        }
+require("dotenv").config();
+const jwt = require("jsonwebtoken");
+ 
+function login(req,res) {
+    try {
+        const privateKey = process.env.JWT_SECRET_KEY
+        const expirationTime = 2*24*60*60
+        const payload = {
+            email:req.body.email,
+            username:req.body.username
+        };
+        const options = {
+            expiresIn: expirationTime
+        };
+        const token = jwt.sign(payload,privateKey,options);
+        console.log(token);
+        res.status(200).json({
+            message: `user ${req.body.username} has successfully logged in`,
+            token: token
+        })
+    }  catch(error) {
+        console.log(error)
+        res.status(500).json(error)
     }
-
-    module.exports = login; 
+}
+ 
+module.exports = login;
--- src/db/middleware/checkPassword.js
+++ src/db/middleware/checkPassword.js
@@ -1,40 +1,37 @@
 const bcrypt = require("bcrypt");
-const { Op } = require("sequelize");
+const {Op} = require("sequelize");
 const User = require("../models/userModel");
-
-async function checkPassword(req, res, next) {
+async function checkPassword(req,res,next) {
     try {
-        
-        const email = req.body.email || "";
-        const username = req.body.username || "";
-
-        
+        if (!req.body.email) {
+            req.body.email = "";
+        }
+        if (!req.body.username) {
+            req.body.username = "";
+        }
         const userDetails = await User.findOne({
-            where: { [Op.or]: [{ email }, { username }] }
-        });
-
-        
+            where: {[Op.or]: [{email: req.body.email}, {username: req.body.username}]}}
+        )
+        console.log(userDetails);
         if (!userDetails) {
-            return res.status(404).json({ error: "User not found" });
+            res.status(404).send("username/email and password do not match");
+            return;
         }
-
-        const plainTextPassword = req.body.password;
-        const hashedPassword = userDetails.dataValues.password;
-
-        
-        const isPasswordCorrect = await bcrypt.compare(plainTextPassword, hashedPassword);
-        if (!isPasswordCorrect) {
-            return res.status(401).json({ error: "Wrong password" });
+        plainTextPassword = req.body.password;
+        hashedPassword = userDetails.dataValues.password;
+        console.log(hashedPassword);
+        const output = await bcrypt.compare(plainTextPassword,hashedPassword);
+        console.log(output);
+        if (!output) {
+            res.status(404).send("username/email and password do not match");
+            return;
         }
-
-        
         req.body.email = userDetails.dataValues.email;
         req.body.username = userDetails.dataValues.username;
-        next();
+        next()
     } catch (error) {
-        console.error(error);
-        res.status(500).json({ error: "Internal server error" });
+        console.log(error);
+        res.status(500).json(error)
     }
 }
-
 module.exports = checkPassword;
