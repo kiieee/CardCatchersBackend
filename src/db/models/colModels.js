beaa0c7602edd10f3242d44184948ef67271ae84
MightySneed <179814389+MightySneed@users.noreply.github.com>
2024-11-15 12:17:36 +0000

    Collections now have usernames

--- src/db/models/colModels.js
+++ src/db/models/colModels.js
@@ -19,6 +19,12 @@ const Col = connection.define("Col", {
         type: DataTypes.STRING,
         unique: false,
         allowNull: false
+    },
+        
+    username: {
+        type: DataTypes.STRING,
+        unique: false,
+        allowNull: false,
     }
 })
 
