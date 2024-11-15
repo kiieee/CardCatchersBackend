6df5f1c8b13cf08da38baa31abb9834969346a45
MightySneed <179814389+MightySneed@users.noreply.github.com>
2024-11-15 09:48:48 +0000

    Y

--- src/db/controllers/addCol.js
+++ src/db/controllers/addCol.js
@@ -3,7 +3,7 @@ async function addCol(req,res) {
     try {
         const output = await Col.create({
             colName: req.body.colName,
-            colGame : req.body.colGame,
+            colGame : req.body.colGame
         })
     console.log(output);
     res.status(200).json({message: `Collection ${req.body.colName} has been created!`})
