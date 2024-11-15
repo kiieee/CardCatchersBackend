9be485f210b6d335dad5cbb448ec096cb14b4c62
MightySneed <179814389+MightySneed@users.noreply.github.com>
2024-11-15 11:46:15 +0000

    delete should work now

--- src/db/controllers/deleteCol.js
+++ src/db/controllers/deleteCol.js
@@ -2,7 +2,7 @@ const Col = require("../models/colModels")
  
 async function deleteCol(req,res) {
     try {
-        await Col.destroy ({where: {colName: req.body.colName}, })
+        await Col.destroy ({where: {colID: req.body.colID}, })
                
             res.status(200).json({message: `Collection ${req.body.colName} has been removed!`})
     } catch (error) {
